4913d1ed644778140fcd1bf76e73383b1cda4ebd
elkin <elkin@recipet.co.kr>
2020-08-26 12:44:42 +0900

    상세 다이어로그 띄우기

--- src/views/pet/index.vue
+++ src/views/pet/index.vue
@@ -8,23 +8,65 @@
       fit
       highlight-current-row
     >
+      <el-table-column label="ID" align="center" width="95">
+        <template slot-scope="{row}">
+          <span class="link-type" @click="handleFetch(row)">{{ row.petId }}</span>
+        </template>
+      </el-table-column>
       <el-table-column
         prop="petName"
         label="애견이름"
+        align="center"
         width="180"
       />
       <el-table-column
         prop="createdAt"
         label="날짜"
+        align="center"
         width="180"
       />
       <el-table-column
         prop="status"
         label="상태"
+        align="center"
         width="180"
       />
     </el-table>
-
+    <el-dialog :visible.sync="dialogPetVisible" title="상세">
+      <el-table :data="pet" border fit highlight-current-row style="width: 100%">
+        <el-table-column prop="petName" label="애견이름" />
+        <el-table-column prop="status" label="status" />
+        <el-table-column prop="createdAt" label="createdAt" />
+        <el-table-column prop="status" label="status" />
+        <el-table-column prop="createdAt" label="createdAt" />
+        <el-table-column prop="status" label="status" />
+        <el-table-column prop="createdAt" label="createdAt" />
+        <el-table-column prop="status" label="status" />
+        <el-table-column prop="createdAt" label="createdAt" />
+        <el-table-column prop="status" label="status" />
+      </el-table>
+      <br><br>
+      <el-table :data="pet" border fit highlight-current-row style="width: 100%">
+        <el-table-column prop="petName" label="애견이름" />
+        <el-table-column prop="status" label="status" />
+        <el-table-column prop="createdAt" label="createdAt" />
+        <el-table-column prop="status" label="status" />
+        <el-table-column prop="createdAt" label="createdAt" />
+      </el-table>
+      <br><br>
+      <el-table :data="pet" border fit highlight-current-row style="width: 100%">
+        <el-table-column prop="petName" label="애견이름" />
+        <el-table-column prop="status" label="status" />
+        <el-table-column prop="status" label="status" />
+        <el-table-column prop="createdAt" label="createdAt" />
+        <el-table-column prop="status" label="status" />
+        <el-table-column prop="createdAt" label="createdAt" />
+        <el-table-column prop="status" label="status" />
+      </el-table>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogPetVisible = false">확인</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -34,6 +76,8 @@ import { fetchList } from '@/api/article'
 export default {
   data() {
     return {
+      dialogPetVisible: false,
+      pet: [],
       list: null,
       total: 0,
       listLoading: true,
@@ -57,6 +101,12 @@ export default {
         this.list = response
         this.listLoading = false
       })
+    },
+    handleFetch(pet) {
+      console.log(pet)
+      this.pet = []
+      this.pet.push(pet)
+      this.dialogPetVisible = true
     }
   }
 }
