857e92a21e6a591a0db57db7487a14048bade07a
elkin <elkin@recipet.co.kr>
2020-08-28 10:36:00 +0900

    로그인 페이지 개선

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -60,7 +60,7 @@
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
+import { validEmail } from '@/utils/validate'
 import SocialSign from './components/SocialSignin'
 
 export default {
@@ -68,15 +68,15 @@ export default {
   components: { SocialSign },
   data() {
     const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+      if (!validEmail(value)) {
+        callback(new Error('Please enter the correct user email'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+      if (value.length < 8) {
+        callback(new Error('The password can not be less than 8 digits'))
       } else {
         callback()
       }
